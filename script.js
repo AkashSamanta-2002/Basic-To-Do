37e03211e32bd876f24a4e14e120729a5d27c1ec
AkashSamanta-2002 <akashsamanta427@gmail.com>
2025-08-16 19:28:39 +0530

    adding all the files

---
+++ script.js
@@ -0,0 +1,45 @@
+let itemContainer = document.querySelector(".itemContainer");
+let btn = document.querySelector("#btn");
+let input = document.querySelector("#input");
+
+let todoList = [];
+
+function renderToDO() {
+    itemContainer.innerHTML = "";
+    todoList.map((obj, idx) => {
+        let divEle = document.createElement("div");
+        divEle.className = "items";
+        divEle.innerHTML = `
+                <div class="todo">${idx+1}. ${obj.text}</div>
+            `;
+        let deleteEle = document.createElement("button");
+        deleteEle.className = "delete";
+        deleteEle.innerText = "❌";
+        divEle.appendChild(deleteEle);
+        itemContainer.append(divEle);
+
+        deleteEle.onclick = () => {
+            handelDelete(obj.id);
+        };
+    });
+}
+
+function handelToDO() {
+    if (input.value == "") return;
+    let obj = {
+        id: Date.now(),
+        text: input.value
+    };
+    todoList.push(obj);
+    renderToDO();
+    input.value = "";
+}
+
+function handelDelete(id) {
+    todoList = todoList.filter((obj) => {
+        return obj.id !== id;
+    });
+    renderToDO();
+}
+
+btn.addEventListener("click", handelToDO);
